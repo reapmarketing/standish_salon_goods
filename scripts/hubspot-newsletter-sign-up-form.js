7f718fe0f50a2fc17339251b9679c77da471b5ad
Lauren Maxwell <lauren@reapmarketing.com>
2015-04-06 16:04:02 -0500

    Reorganizes a bunch of the javascript and throws it in there.

--- scripts/hubspot-newsletter-sign-up-form.js
+++ scripts/hubspot-newsletter-sign-up-form.js
@@ -1,17 +1,25 @@
-// Footer HubSpot newsletter sign up form
-hbspt.forms.create({ 
-	portalId: '239485',
-	formId: '934570fd-43b6-4393-b73a-43cc4bcaea41',
-	css: '',
-	onFormReady: function() {
-		// Clear Placeholder on click of email element
-		$('.hbspt-form input[type=email]').each( function () {
-			if( $(this).val() == 'Enter Email' ) {
-				var element = $(this);
-				element.one( 'click', function() {
-					element.val('');
-				});
-			}
-		});
-	}
+/** Footer HubSpot newsletter sign up form **/
+
+// Load the dom in order to make sure parent hubspot script is loaded first
+// todo -- load script and create an event based on this onload, then use the
+// options for the hubspot form
+document.addEventListener('DOMContentLoaded', function() {
+	hbspt.forms.create({
+		portalId: '239485',
+		formId: '934570fd-43b6-4393-b73a-43cc4bcaea41',
+		css: '',
+		target: '#newsletter-target',
+		onFormReady: function() {
+			console.log('ready');
+			// Clear Placeholder on click of email element
+			$('.hbspt-form input[type=email]').each( function () {
+				if( $(this).val() == 'Enter Email' ) {
+					var element = $(this);
+					element.one( 'click', function() {
+						element.val('');
+					});
+				}
+			});
+		}
+	}).done(function() {console.log('done');});
 });
