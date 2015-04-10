289b20d6fc72f04747032da2664eaef6d6b1e67b
Lauren Maxwell <lauren@reapmarketing.com>
2015-04-10 11:25:37 -0500

    Adds menu based js functionality into seperate file. Adds styles and collapsible functionalityto new mobile menu.

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,133 +1,41 @@
 /** Activation of scripts and app code
  *  @Author Lauren
  **/
-
-
 ;(function($) {
-	$(function(){
-		// Initialize certian groups to have matching heights
-		$('.listing').matchHeight({
-			byRow: true,
-			property: 'height',
-			target: null,
-			remove: false
-		});
-		$('.listing .listing-container').matchHeight({
-			property: 'height',
-			byRow: true,
-			target: null,
-			remove: false
-		});
-		$('.column .eqhgt').matchHeight({
-			property: 'height',
-			byRow: true,
-			target: null,
-			remove: false
-		});
-		$('.column .eqhgt-g1').matchHeight({
-			property: 'height',
-			byRow: true,
-			target: null,
-			remove: false
-		});
+	var Standish = {};
+
+	Standish.EqualHeights = function() {
+		function resizeColumns(groups) {
+			$.each(groups, function(index, value) {
+				$(value).matchHeight({
+					byRow: true,
+					property: 'height',
+					target: null,
+					remove: false
+				});
+			});
+		}
+		var groups = [
+			'.listing',
+			'.listing .listing-container',
+			'.column .eqhgt',
+			'.column .eqhgt-g1'
+		];
+		resizeColumns(groups);
+	}
+	
+	Standish.CollapseMenus = function() {
+			
+	}
+	
+
+	// Events
+	$(function() {
+		Standish.EqualHeights();
+	});
+	// Add callback to window resize event
+	$(window).on('resize', function() {
+		Standish.EqualHeights();
 	});
 	
 })(jQuery);
-
-/* MobileMenu custom class */
-// Get custom menu from menu-options.json and aggregate it into a div container
-(function( MobileMenu, $, undefined ) {
-	
-	MobileMenu.convertToArray = function(data) {
-		var array = [];
-		for ( var key in data ) {
-			array[key] = data[key];
-		}
-		return array;
-	}
-	
-	// Sort the menu items according to object's order property
-	MobileMenu.sortItems = function(menuItems) {
-		return menuItems.sort(function( a , b ) {
-			if (a.order > b.order) {
-				return 1;
-			}
-			if (a.order < b.order) {
-				return -1;	
-			}
-			return 0;	
-		});
-	}
-	
-	MobileMenu.getItems = function() {
-		return $.getJSON('https://json.stand.sh/standish/menu-custom.json', function(data) {
-				
-			var menuItems = [],
-					unList = [];
-			// Convert to array
-			menuItems = MobileMenu.convertToArray(data);
-			// Sort the menu items according to order property
-			menuItems = MobileMenu.sortItems(menuItems);
-			
-			// Traverse through my object
-			for (var key in menuItems) {
-				if (menuItems[key].show_mobile === true) {
-					unList[key]	= '';
-					unList[key]	+= '<li>';
-					if ( menuItems[key].url !== undefined ) {
-						unList[key] += '<a href="' + menuItems[key].url + '">';
-					}
-					else {
-						unList[key] += '<a href="">';
-					}	
-					unList[key] += key + '</a>';
-					if (menuItems[key].subcategories !== undefined) {
-						var subcategories = menuItems[key].subcategories,
-								subcategories = MobileMenu.convertToArray(subcategories),
-								subcategories = MobileMenu.sortItems(subcategories);
-								// subcategories = sortItems(subcategories);
-						
-						unList[key]	+= '<ul>';
-						
-						for (var k in subcategories) {
-							unList[key]	+= '<li>';
-							if ( subcategories[k].url !== undefined ) {
-								unList[key] += '<a href="' + subcategories[k].url + '">';
-							}
-							else {
-								unList[key] += '<a href="">';
-							}	
-							unList[key]	+= k;
-							unList[key]	+= '</a>';
-							unList[key]	+= '</li>';
-						}
-						unList[key]	+= '</ul>';
-						unList[key]	+= '</li>';
-					}
-					else {
-						unList[key]	+= '</li>'
-					}
-				}
-				
-			}
-			unList = MobileMenu.convertToArray(unList);
-			
-			for (var i in unList) {
-				$('#mobile-menu').append(unList[i]);
-			}
-		});
-	}
-})(window.MobileMenu = window.MobileMenu || {}, jQuery);
-
-// Activate My Class
-(function($) {
-	// Document Ready
-	$(function() {
-			MobileMenu.getItems().done(function(context) {
-				$('.toggle-navigation').sidr({
-					source: '#mobile-menu'	
-				});
-				console.log('menu is done :>')});
-	});
-	
-} (jQuery));
---
+++ scripts/menu-behaviors.js
@@ -0,0 +1,120 @@
+/**
+ *  @Author Lurn
+ **/
+/* MobileMenu custom class */
+// Get custom menu from menu-options.json and aggregate it into a div container
+(function( MobileMenu, $, undefined ) {
+
+	convertToArray = function(data) {
+		var array = [];
+		for ( var key in data ) {
+			array[key] = data[key];
+		}
+		return array;
+	}
+
+	// Sort the menu items according to object's order property
+	sortItems = function(menuItems) {
+		return menuItems.sort(function( a , b ) {
+			if (a.order > b.order) {
+				return 1;
+			}
+			if (a.order < b.order) {
+				return -1;	
+			}
+			return 0;	
+		});
+	}
+
+	// todo: create menu walker!
+	// Convert json data into a menu
+	MobileMenu.getItems = function() {
+		return $.getJSON('https://json.stand.sh/standish/menu-custom.json', function(data) {
+
+			var menuItems = [],
+					unList = [];
+			// Convert to array
+			menuItems = convertToArray(data);
+			// Sort the menu items according to order property
+			menuItems = sortItems(menuItems);
+
+			// Traverse through my object
+			for (var key in menuItems) {
+				if (menuItems[key].show_mobile === true) {
+					unList[key]	= '';
+					if (menuItems[key].subcategories !== undefined) {
+						unList[key]	+= '<li class="subcategories">';
+					}
+					else {
+						unList[key]	+= '<li>';
+					}
+					if ( menuItems[key].url !== undefined ) {
+						unList[key] += '<a class="menu-open" href="' + menuItems[key].url + '">';
+					}
+					else {
+						unList[key] += '<a class="menu-open" href="">';
+					}	
+					unList[key] += key + '</a>';
+					if (menuItems[key].subcategories !== undefined) {
+						var subcategories = menuItems[key].subcategories,
+								subcategories = convertToArray(subcategories),
+								subcategories = sortItems(subcategories);
+								// subcategories = sortItems(subcategories);
+						
+						unList[key]	+= '<ul class="mobile-menu hidden menu" style="display:none;">';
+						
+						for (var k in subcategories) {
+							unList[key]	+= '<li>';
+							if ( subcategories[k].url !== undefined ) {
+								unList[key] += '<a href="' + subcategories[k].url + '">';
+							}
+							else {
+								unList[key] += '<a href="">';
+							}	
+							unList[key]	+= k;
+							unList[key]	+= '</a>';
+							unList[key]	+= '</li>';
+						}
+						unList[key]	+= '</ul>';
+						unList[key]	+= '</li>';
+					}
+					else {
+						unList[key]	+= '</li>'
+					}
+				}
+			}
+			unList = convertToArray(unList);
+			$('#mobile-menu').append('<ul />');
+			for (var i in unList) {
+				$('#mobile-menu > ul').append(unList[i]);
+			}
+		});
+	}
+
+	MobileMenu.init = function() {	
+		var el = this;		
+		return el.getItems().done(function(context) {
+			$('.toggle-navigation').sidr({
+				name: 'mobile-open',
+				source: '#mobile-menu'
+			});
+			$('.sidr-inner > ul > li.sidr-class-subcategories').each(function() {
+				var el = this;
+				$(el).on('click', '.sidr-class-menu-open', function(e) {
+					e.preventDefault();
+					$(this).siblings('.sidr-class-mobile-menu').toggle();
+					$(el).siblings().children('.sidr-class-mobile-menu').hide();
+				});
+			});
+		});
+	}
+
+})(window.MobileMenu = window.MobileMenu || {}, jQuery);
+
+// Activate My Class
+(function($) {
+	// Document Ready
+	$(function() {
+		MobileMenu.init();
+	});
+} (jQuery));
