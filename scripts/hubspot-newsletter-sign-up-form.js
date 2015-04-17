55b2bea7909b0877e0e8a3543ee75a7e6fedbcfd
Lauren Maxwell <lauren@reapmarketing.com>
2015-04-17 15:56:32 -0500

    Fixes script selector and wraps in jquery.

--- scripts/hubspot-newsletter-sign-up-form.js
+++ scripts/hubspot-newsletter-sign-up-form.js
@@ -3,7 +3,11 @@
 // Load the dom in order to make sure parent hubspot script is loaded first
 // todo -- load script and create an event based on this onload, then use the
 // options for the hubspot form
-document.addEventListener('DOMContentLoaded', function() {
+
+
+;(function($) {
+	
+	document.addEventListener('DOMContentLoaded', function() {
 	hbspt.forms.create({
 		portalId: '239485',
 		formId: '934570fd-43b6-4393-b73a-43cc4bcaea41',
@@ -11,14 +15,19 @@ document.addEventListener('DOMContentLoaded', function() {
 		target: '#newsletter-target',
 		onFormReady: function() {
 			// Clear Placeholder on click of email element
-			$('.hbspt-form input[type=email]').each( function () {
+			
+		}
+	}).done(function() {
+		$('.hs-form input[type=email]').each( function () {
 				if( $(this).val() == 'Enter Email' ) {
 					var element = $(this);
-					element.one( 'click', function() {
+					element.on( 'click', function() {
 						element.val('');
 					});
 				}
 			});
-		}
 	});
 });
+
+	
+} (jQuery)) 
