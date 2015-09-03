67f0f76f79e7aefeffa3240a572ed1aa16e564b2
Lauren Maxwell <mlmxwell@gmail.com>
2015-09-03 15:39:24 -0500

    Adds additional Versioning support and minifies CSS and adds additional changes requested by tiff.

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,31 @@ module.exports = function(grunt) {
         }
       }
     },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'style',
+          src: ['main.css'],
+          dest: 'style',
+          ext: '.<%= pkg.version %>.min.css'
+        }]
+      }
+    },
+    'string-replace': {
+      version: {
+        files: {
+          'frame.html': 'frame.source.html' // Version out certain files
+        },
+        options: {
+          replacements: [{
+            pattern: /{{ VERSION }}/g,
+            replacement: '<%= pkg.version %>'
+          }]
+        }
+      }
+    },
+    // pkg: grunt.file.readJSON('package.json'),
     watch: {
       sass: {
         files: ['sass/*.scss'],
@@ -28,9 +53,17 @@ module.exports = function(grunt) {
           livereload: true,
         },
       },
+      cssmin: {
+        files: ['<%= cssmin.target.files %>'],
+        tasks: ['cssmin']
+      },
       js: {
         files: ['<%= jshint.files %>'],
         tasks: ['jshint']
+      },
+      strrplc: {
+        files: ['<%= [string-replace].version.files %>'],
+        tasks: ['string-replace']
       },
       options: {
         dateFormat: function(time) {
@@ -45,7 +78,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch'); 
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-notify');
+  grunt.loadNpmTasks('grunt-string-replace');
 
-  grunt.registerTask('default', ['jshint', 'sass']);
+  grunt.registerTask('default', ['jshint', 'sass', 'cssmin', 'string-replace']);
 };
