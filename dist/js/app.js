fa356ae6ec2d44db335a447fad733801abd24921
Lauren Maxwell <lauren@reapmarketing.com>
2016-06-09 09:54:08 -0500

    Removes google analytics.

--- dist/js/app.js
+++ dist/js/app.js
@@ -2445,14 +2445,7 @@ var e=c.find(".active:last a"),f=a.Event("hide.bs.tab",{relatedTarget:b[0]}),g=a
 	// Remove 3DCart clears (use the 'clearfix' class instead)
 	$('div[style*=both]').remove();
 });
-;
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-
-jQuery( function($) {
+;jQuery( function($) {
 
 	// unWrappers
 	$( '.productContainer' ).unwrap();
